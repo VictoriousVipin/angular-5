81c22620659236da82746ca27028beb0d3e71fd4
Singh Tomar <vipin.p.singh.tomar@accenture.com>
2018-06-30 20:36:37 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,53 @@
+//Modules
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+//Components
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LogoutComponent } from './logout/logout.component';
+
+//Services
+import { HomeService } from './home/home.service';
+import { AboutService } from './about/about.service';
+import { ContactService } from './contact/contact.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    LogoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+    },
+    {
+      path: 'home',
+      component: HomeComponent
+    },
+    {
+      path: 'about',
+      component: AboutComponent
+    },
+    {
+      path: 'contact',
+      component: ContactComponent
+    },
+    {
+      path: 'logout',
+      component: LogoutComponent
+    }])
+  ],
+  providers: [HomeService, AboutService, ContactService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
