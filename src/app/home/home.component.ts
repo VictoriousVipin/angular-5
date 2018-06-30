2ff401240e70970b19d2942469ff2ce03ae72884
Singh Tomar <vipin.p.singh.tomar@accenture.com>
2018-06-30 22:28:03 +0530

    adding new interface file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {HomeService} from './home.service';
-import {ApiData} from './home.interface';
+import {ApiData} from '../interfaces/home.interface';
 
 @Component({
   selector: 'app-home',
